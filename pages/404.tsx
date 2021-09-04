f49a1da7a74bd00a1ace9c2095fd0e3953ec9993
William Grant <willmgrant@gmail.com>
2021-09-04 17:31:47 +0200

    removed styling from 404 page

--- pages/404.tsx
+++ pages/404.tsx
@@ -13,17 +13,10 @@ export default function Custom404() {
             'md:flex md:flex-col md:justify-center md:items-center'
           )}
         >
-          <h1
-            className={classNames('text-primary-dark text-5xl font-bold mb-8')}
-          >
+          <h1 className={classNames('text-5xl font-bold mb-8')}>
             This page doesn&apos;t seem to exist.
           </h1>
-          <p
-            className={classNames(
-              'text-gray text-xl font-medium',
-              'lg:text-2xl'
-            )}
-          >
+          <p className={classNames('text-xl font-medium', 'lg:text-2xl')}>
             {METADATA[404].DESCRIPTION}
           </p>
         </div>
