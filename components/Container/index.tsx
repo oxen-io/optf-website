2f045c5781165d45df87fbae400bbf781dc20c1b
William Grant <willmgrant@gmail.com>
2021-10-28 09:59:08 +1100

    added container component

---
+++ components/Container/index.tsx
@@ -0,0 +1,37 @@
+import React, { ReactNode } from 'react';
+
+import UI from '@/constants/ui';
+import classNames from 'classnames';
+
+interface Props {
+  id?: string;
+  fullWidth?: boolean;
+  classes?: string;
+  children: ReactNode;
+}
+
+export const containerStyles = {
+  paddingLeft: `${UI.CONTAINER_PADDING_X}vw`,
+  paddingRight: `${UI.CONTAINER_PADDING_X}vw`,
+  maxWidth: `${UI.CONTAINER_MAX_WIDTH}px`,
+};
+
+export function Contained(props: Props) {
+  const { id, fullWidth = false, classes, children } = props;
+
+  return (
+    <div className={classNames('w-full')}>
+      <div
+        id={id}
+        className={classNames('w-full my-0 mx-auto', classes)}
+        style={{
+          paddingLeft: !fullWidth ? containerStyles.paddingLeft : undefined,
+          paddingRight: !fullWidth ? containerStyles.paddingRight : undefined,
+          maxWidth: !fullWidth ? containerStyles.maxWidth : undefined,
+        }}
+      >
+        {children}
+      </div>
+    </div>
+  );
+}
