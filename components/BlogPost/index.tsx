d8934d2a5539fb04bc05d9982bcb8cf13ad38cca
tomobre <tomobre@gmail.com>
2023-04-06 15:16:50 +1000

    fix: <></> not needed

--- components/BlogPost/index.tsx
+++ components/BlogPost/index.tsx
@@ -13,29 +13,26 @@ interface Props {
 export default function BlogPost(props: Props): ReactElement {
   const { post } = props;
   return (
-    <>
-      <Layout
-        title={post.title}
-        metadata={{
-          TYPE: 'article',
-          DESCRIPTION: post.description,
-          CANONICAL_URL: `${METADATA.HOST_URL}/blog`,
-          OG_IMAGE: {
-            URL: post.featureImage?.imageUrl ?? METADATA.OG_IMAGE.URL,
-            WIDTH: Number(post.featureImage?.width) ?? METADATA.OG_IMAGE.WIDTH,
-            HEIGHT:
-              Number(post.featureImage?.height) ?? METADATA.OG_IMAGE.HEIGHT,
-            ALT: post.featureImage?.title ?? METADATA.OG_IMAGE.ALT,
-          },
-          TAGS: post.tags,
-          ARTICLE_SECTION: post.tags[0],
-          PUBLISHED_TIME: post.publishedDateISO,
-        }}
-      >
-        <div className="pt-10 bg-gray-250 md:pt-10">
-          <Post {...props} />
-        </div>
-      </Layout>
-    </>
+    <Layout
+      title={post.title}
+      metadata={{
+        TYPE: 'article',
+        DESCRIPTION: post.description,
+        CANONICAL_URL: `${METADATA.HOST_URL}/blog`,
+        OG_IMAGE: {
+          URL: post.featureImage?.imageUrl ?? METADATA.OG_IMAGE.URL,
+          WIDTH: Number(post.featureImage?.width) ?? METADATA.OG_IMAGE.WIDTH,
+          HEIGHT: Number(post.featureImage?.height) ?? METADATA.OG_IMAGE.HEIGHT,
+          ALT: post.featureImage?.title ?? METADATA.OG_IMAGE.ALT,
+        },
+        TAGS: post.tags,
+        ARTICLE_SECTION: post.tags[0],
+        PUBLISHED_TIME: post.publishedDateISO,
+      }}
+    >
+      <div className="pt-10 bg-gray-250 md:pt-10">
+        <Post {...props} />
+      </div>
+    </Layout>
   );
 }
