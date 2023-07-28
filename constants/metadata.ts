0c0c1b14f435dfdd8fb9466100009c9677122fcf
William Grant <willmgrant@gmail.com>
2023-07-28 10:16:44 +1000

    feat: compressed all images and updated link preview image to separete file

--- constants/metadata.ts
+++ constants/metadata.ts
@@ -34,7 +34,7 @@ const METADATA = {
   ],
   OG_TYPE: 'website',
   OG_IMAGE: {
-    URL: '/assets/images/logo-optf.png',
+    URL: '/site-banner.png',
     WIDTH: 260,
     HEIGHT: 85,
     ALT: 'OPTF logo',
@@ -58,154 +58,70 @@ const METADATA = {
     TYPE: 'article',
     DESCRIPTION:
       'OPTF | Building a world where the internet is open, software is free, and privacy is protected. Join us in the fight for a free, fair, and equal internet.',
-    OG_IMAGE: {
-      URL: '/assets/images/logo-optf.png',
-      WIDTH: 260,
-      HEIGHT: 85,
-      ALT: 'OPTF logo',
-    },
   },
   BLOG_PAGE: {
     TYPE: 'article',
     DESCRIPTION:
       'Blog | Latest news, articles, and updates on privacy, free software, and open internet. Join the conversation on building a safer digital world.',
-    OG_IMAGE: {
-      URL: '/assets/images/logo-optf.png',
-      WIDTH: 260,
-      HEIGHT: 85,
-      ALT: 'OPTF logo',
-    },
   },
   PROJECTS: {
     TYPE: 'article',
     DESCRIPTION:
       'OPTF | We believe that digital technologies must give people privacy by default.',
-    OG_IMAGE: {
-      URL: '/assets/images/logo-optf.png',
-      WIDTH: 260,
-      HEIGHT: 85,
-      ALT: 'OPTF logo',
-    },
   },
   RESEARCH: {
     TYPE: 'article',
     DESCRIPTION:
       'Research | Learn about our efforts to promote privacy, free software, and open internet through innovative research.',
-    OG_IMAGE: {
-      URL: '/assets/images/logo-optf.png',
-      WIDTH: 260,
-      HEIGHT: 85,
-      ALT: 'OPTF logo',
-    },
   },
   CONTACT: {
     TYPE: 'article',
     DESCRIPTION:
       'Contact Us | Connect with us to learn more about our mission and services. Share your thoughts, feedback, or inquiries.',
-    OG_IMAGE: {
-      URL: '/assets/images/logo-optf.png',
-      WIDTH: 260,
-      HEIGHT: 85,
-      ALT: 'OPTF logo',
-    },
   },
   PRIVACY_POLICY: {
     TYPE: 'article',
     DESCRIPTION:
       "Privacy Policy | Learn about OPTF's commitment to protecting your privacy with our Privacy Policy.",
-    OG_IMAGE: {
-      URL: '/assets/images/logo-optf.png',
-      WIDTH: 260,
-      HEIGHT: 85,
-      ALT: 'OPTF logo',
-    },
   },
   TRANSPARENCY: {
     TYPE: 'article',
     DESCRIPTION: 'Transparency Report | OPTF',
-    OG_IMAGE: {
-      URL: '/assets/images/logo-optf.png',
-      WIDTH: 260,
-      HEIGHT: 85,
-      ALT: 'OPTF logo',
-    },
   },
   FEEDBACK: {
     TYPE: 'article',
     DESCRIPTION:
       'Feedback | Have feedback or suggestions on how we can improve our mission and services? Share your thoughts with us! ',
-    OG_IMAGE: {
-      URL: '/assets/images/logo-optf.png',
-      WIDTH: 260,
-      HEIGHT: 85,
-      ALT: 'OPTF logo',
-    },
   },
   ABOUT: {
     TYPE: 'article',
     DESCRIPTION:
       'About the OPTF | Learn about our mission, meet our team, and explore technology projects to build a safer and more secure digital world.',
-    OG_IMAGE: {
-      URL: '/assets/images/logo-optf.png',
-      WIDTH: 260,
-      HEIGHT: 85,
-      ALT: 'OPTF logo',
-    },
   },
   PARTNERS_AND_ALLIES: {
     TYPE: 'article',
     DESCRIPTION:
       "Partners and Allies | Discover the organisations and allies supporting OPTF's mission for a free and open internet.",
-    OG_IMAGE: {
-      URL: '/assets/images/logo-optf.png',
-      WIDTH: 260,
-      HEIGHT: 85,
-      ALT: 'OPTF logo',
-    },
   },
   LEGALS: {
     TYPE: 'article',
     DESCRIPTION:
       'Legals | Stay informed and up-to-date on the OPTF’s legal policies as we work to build a safer and more secure digital world.',
-    OG_IMAGE: {
-      URL: '/assets/images/logo-optf.png',
-      WIDTH: 260,
-      HEIGHT: 85,
-      ALT: 'OPTF logo',
-    },
   },
   ANNUAL_REPORTS: {
     TYPE: 'article',
     DESCRIPTION:
       'Annual Reports | Stay informed and learn about our achievements as we work towards building a safer and more secure digital world.',
-    OG_IMAGE: {
-      URL: '/assets/images/logo-optf.png',
-      WIDTH: 260,
-      HEIGHT: 85,
-      ALT: 'OPTF logo',
-    },
   },
   FUNDING: {
     TYPE: 'article',
     DESCRIPTION:
       "Funding | Apply for funding from from the OPTF to support your project or organisation's mission to promote privacy, free software, and open internet.",
-    OG_IMAGE: {
-      URL: '/assets/images/logo-optf.png',
-      WIDTH: 260,
-      HEIGHT: 85,
-      ALT: 'OPTF logo',
-    },
   },
   DONATIONS: {
     TYPE: 'article',
     DESCRIPTION:
       "Donations | Support the OPTF's mission to promote privacy, free software, and open internet by making a donation.",
-    OG_IMAGE: {
-      URL: '/assets/images/logo-optf.png',
-      WIDTH: 260,
-      HEIGHT: 85,
-      ALT: 'OPTF logo',
-    },
   },
 };
 
