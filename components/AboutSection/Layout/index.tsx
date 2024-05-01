e0213bdd04c73d24593d10c57a9d86a2b314d9ac
Ryan Miller <ryan.m@getsession.org>
2024-05-01 14:17:06 +1000

    feat: change about tab links to next link

--- components/AboutSection/Layout/index.tsx
+++ components/AboutSection/Layout/index.tsx
@@ -1,13 +1,13 @@
 import Banner from '@/components/Banner';
 import Container from '@/components/Container';
 import classNames from 'classnames';
-import { useRouter } from 'next/router';
 import { AboutPageTab } from '@/pages/[slug]';
 import { CMSLegals, CMSPage } from '@/types/cms';
 import RichBody from '@/components/RichBody';
 import { Layout } from '@/components/ui';
 import { parseMetadata } from '@/components/CustomHead';
 import SpecialTabContent from '../Tabs/SpecialTabContent';
+import Link from 'next/link';
 
 type Props = {
   page: CMSPage;
@@ -17,7 +17,6 @@ type Props = {
 
 export default function AboutLayout(props: Props) {
   const { page, tabs, items } = props;
-  const router = useRouter();
   return (
     <Layout title={page.title} metadata={parseMetadata(page)}>
       <Banner
@@ -28,20 +27,18 @@ export default function AboutLayout(props: Props) {
         <Container classes="container grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 lg:text-lg bg-white">
           {tabs.map(({ title, slug }, index) => {
             return (
-              <h3
-                key={title + index}
-                className={classNames(
-                  'mr-7 pb-2 cursor-pointer lg:my-0 lg:border-b-3  lg:hover:border-b-violet-250 lg:pt-0.5',
-                  slug === page.slug
-                    ? 'border-b-3 border-b-violet-250'
-                    : 'border-b-[rgb(0,0,0,0)]'
-                )}
-                onClick={() => {
-                  router.push(slug);
-                }}
-              >
-                {title}
-              </h3>
+              <Link key={title + index} href={slug} passHref prefetch>
+                <h3
+                  className={classNames(
+                    'mr-7 pb-2 cursor-pointer lg:my-0 lg:border-b-3 lg:hover:border-b-violet-250 lg:pt-0.5',
+                    slug === page.slug
+                      ? 'border-b-3 border-b-violet-250'
+                      : 'border-b-[rgb(0,0,0,0)]'
+                  )}
+                >
+                  {title}
+                </h3>
+              </Link>
             );
           })}
         </Container>
